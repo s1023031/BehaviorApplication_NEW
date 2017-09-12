5553c97eddf3c397b8f6e44f368782b0bd772fd6
xxcqc250 <a0963272507@gmail.com>
2017-09-12 15:51:09 +0800

    重新改寫

--- app/src/main/java/com/behavior/ming_yi/AppParser/EBCnewsParser.java
+++ app/src/main/java/com/behavior/ming_yi/AppParser/EBCnewsParser.java
@@ -27,58 +27,29 @@ public class EBCnewsParser extends AppTempleteParser {
         StringBuilder data = new StringBuilder();
         List<AccessibilityNodeInfo> CacheNodes = mAccessibilityNodeInfo.findAccessibilityNodeInfosByViewId("com.ebc.news:id/coordinatorLayout");
 
-        if(CacheNodes.size()==0){
-            return null;
-        }
-        AccessibilityNodeInfo FinalNode = SerachClassName(CacheNodes.get(0),"android.webkit.WebView");
-        if(FinalNode == null) return null;
-        if(FinalNode.getChildCount() == 0) return null;
-        if(FinalNode.getChild(0).getChildCount() < 6);
-        StringBuilder title = new StringBuilder() ;
-        StringBuilder str_context = new StringBuilder();
+        if(CacheNodes.size()==0)return null;
 
-        if(FinalNode.getChild(0).getContentDescription() != null)
-        {
-//            Log.e("APP_OO",FinalNode.getChild(0).toString());
-            title.append(FinalNode.getChild(0).getContentDescription().toString());
-        }
-
-        if(FinalNode.getChild(0).getChild(6) != null)
-            str_context.append(FinalNode.getChild(0).getChild(6) .getContentDescription()+"\n");
-        for(int i=0 ; i<FinalNode.getChild(0).getChild(6).getChildCount();i++){
-            AccessibilityNodeInfo tmpNode = FinalNode.getChild(0).getChild(6).getChild(i);
-            if (tmpNode.getContentDescription()!=null && tmpNode.getContentDescription().length()!=0){
-                if (tmpNode.getContentDescription().toString().contains("【今日最熱門】"))
-                    break;
-                str_context.append(tmpNode.getContentDescription().toString()+"\n");
-            }
-        }
-        if(title !=null && str_context != null){
-            data.append(title+"\n");
-            data.append(str_context);
-        }
+        AccessibilityNodeInfo bodyNode=CacheNodes.get(0);
+        data=SearchClassName(bodyNode,"android.view.View",data);
 
         Log.i(TAG,"EBCnews => "+data.toString());
         return data.toString();
     }
 
-    private AccessibilityNodeInfo SerachClassName(AccessibilityNodeInfo CacheNode,String name){
-        AccessibilityNodeInfo data = null;
-
-        int CacheNodechildcount = CacheNode.getChildCount();
-        String vv = Integer.toString(CacheNodechildcount);
-
-        Log.e("HAAA",vv);
-        if(CacheNode.getClassName() != null && CacheNode.getClassName().equals(name)) {
-            Log.e("BBBB",CacheNode.toString());
-            return CacheNode;
+    private StringBuilder SearchClassName(AccessibilityNodeInfo CacheNode,String name,StringBuilder data){
+        if(CacheNode.getClassName().equals(name) && CacheNode.getContentDescription()!=null)
+        {
+            data.append(CacheNode.getContentDescription().toString());
         }
 
-        for(int i=0; i<CacheNodechildcount ; i++){
-            if(data!=null) break;
-            data = SerachClassName(CacheNode.getChild(i),name);
+        if(CacheNode.getChildCount()>0)
+        {
+            for(int i=0;i<CacheNode.getChildCount();i++)
+            {
+                data=(SearchClassName(CacheNode.getChild(i),name,data));
+            }
         }
-
         return data;
     }
+
 }
